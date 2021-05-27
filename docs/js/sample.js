f7f48d4fdb5897878c1ac43c5242e52e7f643155
natravous <67213861+natravous@users.noreply.github.com>
2021-05-27 15:24:44 +0700

    adding Add Page and Edit Page

--- docs/js/sample.js
+++ docs/js/sample.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
-  });
+});
+
+// text input
+$(document).ready(function() {
+  $('input#input_text, textarea#textarea2').characterCounter();
+});
