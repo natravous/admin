53e77d719daaaa605ed6183d148b49d169721d3b
natravous <67213861+natravous@users.noreply.github.com>
2021-06-05 15:09:44 +0700

    update CRUD

--- docs/js/script.js
+++ docs/js/script.js
@@ -9,134 +9,163 @@ var firebaseConfig = {
     messagingSenderId: "412262375065",
     appId: "1:412262375065:web:af929785b8e1c873e5e208",
     measurementId: "G-CQ907QF5E6"
-  };
-  // Initialize Firebase
-  if(!firebase.apps.length){
-    firebase.initializeApp(firebaseConfig);
-    firebase.analytics();
-  }
-  
-  var database = firebase.database();
-  var storage = firebase.storage();
-  
-  //read
-  // database.ref("/").on("value", (data)=>{
-  //     const dt = data.val();
-  //     console.log(dt);
-  // })
-  
-  //write
-  function kirim(){
-    var date = new Date().getTime();
-    var data = {iden: date,
+};
+// Initialize Firebase
+if(!firebase.apps.length){
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+}
+
+var database = firebase.database();
+var storage = firebase.storage();
+
+//read
+// database.ref("/").on("value", (data)=>{
+//     const dt = data.val();
+//     console.log(dt);
+// })
+
+
+const rupiah = (value) => {
+    let keluaran = ""
+    let keluaran2 =""
+    let counter = 0;
+    for (var i =0; i< value.length; i++){
+      counter++;
+      if(counter == 3 && i < (value.length -1)){
+        keluaran += value.charAt(value.length -i -1)
+        keluaran += "."
+        counter =0;
+      }else{
+        keluaran += value.charAt(value.length - i -1)
+      }
+    }
+    keluaran2 = reverseString(keluaran);
+    return keluaran2;
+}
+
+const reverseString = (str)=> {
+    return str.split("").reverse().join("");
+}
+
+
+//write
+function kirim(){
+var date = new Date().getTime();
+var data = {iden: date,
+nama: document.getElementById("nama").value,
+harga: document.getElementById("harga").value,
+deskripsi: document.getElementById("deskripsi").value}
+database.ref(`/makanan/${date}`).set(data).then(console.log('kirimed'))
+}
+
+//read
+database.ref("/makanan").on("value", (dtman)=>{
+let tampil = dtman.val();
+var card = document.getElementById("card-menu");
+let dataht = "";
+// var kunci = document.getElementById("identitas").value = dtman.val().iden;
+for(key in tampil){
+    let a = tampil[key].nama;
+    let b = rupiah(tampil[key].harga);
+    let c = tampil[key].deskripsi;
+    let d = tampil[key].iden;
+    dataht += `<div class="col l4 s12 m6">
+                <div class="card">
+                    <div class="card-image bluish">
+                        <img src="">
+                        <span class="card-title pgn right-align">Rp. ${b}</span>
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">${a}</span>
+                        <p>${c}</p>
+                    </div>
+                    <div class="card-action">
+                        <a class="waves-effect waves-light modal-trigger" href="#modal1" onclick="edit(${key})">EDIT</a>
+                        <a class="pinggir"  onclick="hapus(${key})">DELETE</a>
+                    </div>
+                </div>
+            </div>`;
+    console.log(card);
+}
+card.innerHTML = dataht
+})
+
+//delete
+function hapus(key){
+database.ref("/makanan/"+key).remove();
+}
+// database.ref("/makanan").remove();
+
+//edit data
+function edit(key){
+database.ref("/makanan/"+key).on("value", (datae)=>{
+//   let tampl = datae.val();
+    
+    document.getElementById("nama").value = datae.val().nama;
+    document.getElementById("harga").value = datae.val().harga;
+    document.getElementById("deskripsi").value = datae.val().deskripsi;
+    
+//   database.ref(`/makanan/${key}`).update({
+//     iden: key,
+//     nama: document.getElementById("nama").value,
+//     harga: document.getElementById("harga").value,
+//     deskripsi: document.getElementById("deskripsi").value
+
+    
+//   });
+
+    //untuk menyimpan  key sementara
+    document.getElementById("editNow").innerHTML = key;
+    console.log(key);
+
+});
+}
+
+//trigger edit data
+function tombolEdit(){
+let key = document.getElementById("editNow").innerHTML;
+database.ref(`/makanan/${key}`).update({
+    iden: key,
     nama: document.getElementById("nama").value,
     harga: document.getElementById("harga").value,
-    deskripsi: document.getElementById("deskripsi").value}
-    database.ref(`/makanan/${date}`).set(data).then(console.log('kirimed'))
-  }
-  
-  //read
-  database.ref("/makanan").on("value", (dtman)=>{
-    let tampil = dtman.val();
-    var card = document.getElementById("card-menu");
-    let dataht = "";
-    // var kunci = document.getElementById("identitas").value = dtman.val().iden;
-    for(key in tampil){
-      let a = tampil[key].nama;
-      let b = tampil[key].harga;
-      let c = tampil[key].deskripsi;
-      let d = tampil[key].iden;
-      dataht += `<div class="col l4 s12 m6">
-                    <div class="card">
-                        <div class="card-image bluish">
-                            <img src="">
-                            <span class="card-title pgn right-align">Rp. ${b}</span>
-                        </div>
-                        <div class="card-content">
-                            <span class="card-title">${a}</span>
-                            <p>${c}</p>
-                        </div>
-                        <div class="card-action">
-                            <a class="waves-effect waves-light btn modal-trigger" href="#modal1" onclick="edit(${key})">EDIT</a>
-                            <a class="pinggir"  onclick="hapus(${key})">DELETE</a>
-                        </div>
-                    </div>
-                </div>`;
-      console.log(card);
-    }
-    card.innerHTML = dataht
-  })
-  
-  function hapus(key){
-    database.ref("/makanan/"+key).remove();
-  }
-  // database.ref("/makanan").remove();
-  
-  function edit(key){
-    database.ref("/makanan/"+key).on("value", (datae)=>{
-    //   let tampl = datae.val();
-        
-      document.getElementById("nama").value = datae.val().nama;
-      document.getElementById("harga").value = datae.val().harga;
-      document.getElementById("deskripsi").value = datae.val().deskripsi;
-      
-    //   database.ref(`/makanan/${key}`).update({
-    //     iden: key,
-    //     nama: document.getElementById("nama").value,
-    //     harga: document.getElementById("harga").value,
-    //     deskripsi: document.getElementById("deskripsi").value
-    
-        
-    //   });
-        document.getElementById("editNow").innerHTML = key;
-      console.log(key);
-  
-    });
-  }
-  
-  function tombolEdit(){
-    let key = document.getElementById("editNow").innerHTML;
-    database.ref(`/makanan/${key}`).update({
-      iden: key,
-      nama: document.getElementById("nama").value,
-      harga: document.getElementById("harga").value,
-      deskripsi: document.getElementById("deskripsi").value
-    }).then(document.getElementById("editNow").innerHTML = "");
-  }
-  
-  //login
-  function login(){
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION).then(() => {
-      return firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(success => {
-        console.log("Hey");
-      })
-      .catch(error => {
-        console.log("Gagal");
-      })
-    }).catch(error => {
-      console.log(error.message);
+    deskripsi: document.getElementById("deskripsi").value
+}).then(document.getElementById("editNow").innerHTML = "");
+}
+
+//login
+function login(){
+let email = document.getElementById("email").value;
+let password = document.getElementById("password").value;
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION).then(() => {
+    return firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(success => {
+    console.log("Hey");
     })
-  
-  
-    // firebase.auth().signInWithEmailAndPassword(email, password)
-    // .then((userCredential) => {
-    //   // Signed in
-    //   var user = userCredential.user;
-    //   // ...
-    // })
-    // .catch((error) => {
-    //   var errorCode = error.code;
-    //   var errorMessage = error.message;
-    // });
-  }
-  
-  // logout
-  function logout(){
-    firebase.auth().signOut().then(success =>{
-      console.log("logout");
+    .catch(error => {
+    console.log("Gagal");
     })
-    
-  }
+}).catch(error => {
+    console.log(error.message);
+})
+
+
+// firebase.auth().signInWithEmailAndPassword(email, password)
+// .then((userCredential) => {
+//   // Signed in
+//   var user = userCredential.user;
+//   // ...
+// })
+// .catch((error) => {
+//   var errorCode = error.code;
+//   var errorMessage = error.message;
+// });
+}
+
+//logout
+function logout(){
+firebase.auth().signOut().then(success =>{
+    console.log("logout");
+})
+
+}
